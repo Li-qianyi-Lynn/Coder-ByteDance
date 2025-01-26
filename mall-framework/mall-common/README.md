82d9dc6dd9547fcd94d16d2ce447dd39c779e294
gaoyx <gao.yx.apply@outlook.com>
2025-01-26 20:52:25 +0800

    Initial commit

---
+++ mall-framework/mall-common/README.md
@@ -0,0 +1,4 @@
+## 本模块为基础模块，包括基本异常和Response
+## 用法
+### Response:
+统一使用 `Response.success()` 方法 和 `Response.fail()` 方法来返回结果。
---
+++ mall-framework/mall-common/src/main/java/com/coder/common/util/JsonUtils.java
@@ -0,0 +1,42 @@
+package com.coder.common.util;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
+import org.apache.commons.lang3.StringUtils;
+
+public class JsonUtils {
+
+    private static ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
+    /** SpringMVC 中自带一个 ObjectMapper，直接读取配置好的 ObjectMapper */
+    public static void init(ObjectMapper objectMapper) {
+        OBJECT_MAPPER = objectMapper;
+    }
+
+    /**
+     * 将对象转为 Json 字符串
+     * @param obj 传入对象
+     * @return Json 字符串
+     */
+    @SneakyThrows
+    public static String toJsonString(Object obj) {
+        return OBJECT_MAPPER.writeValueAsString(obj);
+    }
+
+    /**
+     * 将 JSON 字符串转换为对象
+     *
+     * @param jsonStr
+     * @param clazz
+     * @return
+     * @param <T>
+     */
+    @SneakyThrows
+    public static <T> T parseObject(String jsonStr, Class<T> clazz) {
+        if (StringUtils.isBlank(jsonStr)) {
+            return null;
+        }
+
+        return OBJECT_MAPPER.readValue(jsonStr, clazz);
+    }
+}

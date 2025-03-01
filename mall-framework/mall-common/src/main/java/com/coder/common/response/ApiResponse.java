24d4e05c66eb859f610b4b82626f808b1356a99e
tjh <tjh@qq.com>
2025-03-01 18:10:49 +0800

    check-out基本功能的优化2

---
+++ mall-framework/mall-common/src/main/java/com/coder/common/response/ApiResponse.java
@@ -0,0 +1,33 @@
+package com.coder.common.response;
+
+// src/main/java/com/coder/mall/common/dto/ApiResponse.java
+
+
+import lombok.Data;
+
+@Data
+public class ApiResponse<T> {
+    private boolean success;
+    private int code;
+    private String message;
+    private T data;
+
+    public static <T> ApiResponse<T> success(T data) {
+        ApiResponse<T> response = new ApiResponse<>();
+        response.setCode(200);
+        response.setMessage("success");
+        response.setData(data);
+        return response;
+    }
+
+    public static <T> ApiResponse<T> error(String message) {
+        ApiResponse<T> response = new ApiResponse<>();
+        response.setCode(500);
+        response.setMessage(message);
+        return response;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+}
